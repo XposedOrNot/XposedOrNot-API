4c322ea7c269f0eedbd8c21df11e4e95d384cd72
DevaOnBreaches <devaonbreaches@gmail.com>
2023-12-09 17:19:43 +0530

    ♻️ refactor: Shortened few more long lines to comply with Pylint recommendations

--- cloudflare.py
+++ cloudflare.py
@@ -28,7 +28,8 @@ def update_cf_trans(ip_address):
     ip_address (str): The IP address to be logged in the transaction.
 
     Returns:
-    None: The function doesn't return anything but updates the datastore with the provided IP address.
+    None: The function doesn't return anything but updates the datastore with the provided
+    IP address.
     """
     key = hashlib.md5(ip_address).hexdigest()
     datastore_client = datastore.Client()
@@ -51,15 +52,16 @@ def block_hour(ip_address):
     Block an IP address for one hour using the Cloudflare API.
 
     This function sends a POST request to the Cloudflare API to block the given IP address.
-    It uses predefined authentication details (AUTH_EMAIL and AUTH_KEY) and sets the mode to 'challenge',
-    targeting the specified IP address. The function logs the action by calling `update_cf_trans`
-    with the response content if the request is successful.
+    It uses predefined authentication details (AUTH_EMAIL and AUTH_KEY) and sets the mode to
+    'challenge',    targeting the specified IP address. The function logs the action by
+    calling `update_cf_trans` with the response content if the request is successful.
 
     Parameters:
     ip_address (str): The IP address to be blocked.
 
     Returns:
-    None: The function doesn't return anything but performs an API request to block an IP address and logs the transaction.
+    None: The function doesn't return anything but performs an API request to block an IP
+    address and logs the transaction.
     """
     url = "https://api.cloudflare.com/client/v4/user/firewall/access_rules/rules"
     headers = {
@@ -82,15 +84,16 @@ def block_day(ip_address):
     Block an IP address for one day using the Cloudflare API.
 
     This function sends a POST request to the Cloudflare API to block the given IP address.
-    It uses predefined authentication details (AUTH_EMAIL and AUTH_KEY) and sets the mode to 'challenge',
-    targeting the specified IP address. The function logs the action by calling `update_cf_trans`
-    with the response content if the request is successful.
+    It uses predefined authentication details (AUTH_EMAIL and AUTH_KEY) and sets the mode
+    to 'challenge', targeting the specified IP address. The function logs the action by
+    calling `update_cf_trans` with the response content if the request is successful.
 
     Parameters:
     ip_address (str): The IP address to be blocked.
 
     Returns:
-    None: The function doesn't return anything but performs an API request to block an IP address and logs the transaction.
+    None: The function doesn't return anything but performs an API request to block an
+    IP address and logs the transaction.
     """
     url = "https://api.cloudflare.com/client/v4/user/firewall/access_rules/rules"
     headers = {
@@ -104,7 +107,6 @@ def block_day(ip_address):
         + '"},"notes":"Day block enforced"}'
     )
     response = requests.post(url, headers=headers, data=payload)
-    # update_cf_trans(response.content)
     if response.status_code in [200, 201]:
         update_cf_trans(response.content)
 
@@ -113,10 +115,11 @@ def unblock():
     """
     Unblocks IP addresses that have been blocked for over an hour using the Cloudflare API.
 
-    This function iterates through entities in a Cloud Firestore database, identified by the 'xon_cf' kind,
-    looking for entities without a release timestamp. It then checks if the blocked IP address has been blocked
-    for more than an hour. If so, it sends a DELETE request to the Cloudflare API to unblock the IP address
-    and updates the entity in the Cloud Firestore with the current UTC timestamp as the release timestamp.
+    This function iterates through entities in a Cloud Firestore database, identified by
+    the 'xon_cf' kind,looking for entities without a release timestamp. It then checks if
+    the blocked IP address has been blocked for more than an hour. If so, it sends a DELETE
+    request to the Cloudflare API to unblock the IP address and updates the entity in the
+    Cloud Firestore with the current UTC timestamp as the release timestamp.
 
     Returns:
     bool: Returns True if the function completes execution without errors.
@@ -144,7 +147,9 @@ def unblock():
             response = requests.request("DELETE", url, headers=headers)
             if response.status_code != 200:
                 raise Exception(
-                    f"Failed to delete firewall rule: {firewall_rule_id}. Response code: {response.status_code}, Response: {response.text}"
+                    f"Failed to delete firewall rule: {firewall_rule_id}. "
+                    f"Response code: {response.status_code}, "
+                    f"Response: {response.text}"
                 )
             entity.update({"release_timestamp": datetime.datetime.utcnow().isoformat()})
             datastore_client.put(entity)
